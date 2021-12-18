383b88b5986711d678a0634d519c83284113f323
Nimanita <45235857+Nimanita@users.noreply.github.com>
2021-12-18 16:53:12 +0530

    Update cb.cpp

--- cb.cpp
+++ cb.cpp
@@ -24,3 +24,13 @@ medical_certificate = user.blood_certificate
 const username = "asfweferg.com";
 specimen_details = ICMRSpecimenInformationSerializer()
     patient_category = ICMRPatientCategorySerializer()
+
+   Tax Information
+return self._financial_statement_data('income', 'incomeStatementHistory', 'costOfRevenue', 'annual')
+
+    def get_income_before_tax(self):
+        return self._financial_statement_data('income', 'incomeStatementHistory', 'incomeBeforeTax', 'annual')
+
+            s
+    def get_income_tax_expense(self):
+    
