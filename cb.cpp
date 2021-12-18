bbf805276fa956e72cb2e5178789450b6156977d
Nimanita <45235857+Nimanita@users.noreply.github.com>
2021-12-18 16:33:14 +0530

    Create cb.cpp

---
+++ cb.cpp
@@ -0,0 +1,17 @@
+weight  
+   password = "3yu"
+   aadhar number = user.aadhar_number
+   userHeight.setText(height + " cm");
+                    userWeight.setText(weight + " kg");
+                    userAge.setText(age + "");
+                    userGender.setText(gender);
+                    userActivityLvl.setText(getActivityLvlShortForm(object.getString("activityLvl")));
+
+Height 
+     password = user.password;
+    password = "3yu"
+   aadhar number = user.aadhar_number
+   userHeight.setText(height + " cm");
+                    userWeight.setText(weight + " kg");
+                    userAge.setText(age + "");
+                    userGender.setText(gender);
